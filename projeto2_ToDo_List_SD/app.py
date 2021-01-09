50bdcaa493e8908887cfbbe6aebac7e99da75202
luizfernando <luiz-fsilva37@hotmail.com>
2021-01-09 14:15:25 -0300

    Atualização projeto 2 SD

--- projeto2_ToDo_List_SD/app.py
+++ projeto2_ToDo_List_SD/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"Mensagem": "Bem vindo(a) ao ToDo List API!"})
+    return jsonify({"Mensagem": "Bem vindo(a) ao ToDo List!"})
 
 
 @app.route("/tarefas")
