d77f0a2bc4c1c14b32a5e549961a9d988b4c983b
luizfernando <luiz-fsilva37@hotmail.com>
2021-01-10 16:02:39 -0300

    Atualização projeto 2 SD

--- projeto2_ToDo_List_SD/tasks_list.py
+++ projeto2_ToDo_List_SD/tasks_list.py
@@ -1,5 +1,5 @@
 tasks = [
     {"id": 1, "descricao": "Fazer exercicios fisicos", "status": "concluida"},
     {"id": 2, "descricao": "Assistir videoaulas", "status": "pendente"},
-    {"id": 3, "descricao": "Resolver lista exercicios", "status": "pendente"},
+    {"id": 3, "descricao": "Resolver lista de exercicios", "status": "pendente"},
 ]
