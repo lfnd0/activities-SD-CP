057b0b9cb87d49567da2eebc102c8020ccee60d8
luizfernando <luiz-fsilva37@hotmail.com>
2021-02-20 21:27:26 -0300

    Atualização atividades SD/CP

--- projeto1_IMC_SD/src/CalculadoraIMCIF.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface CalculadoraIMCIF extends Remote {
-
-	public double calcularIMC(double altura, double peso) throws RemoteException;
-
-	public String interpretarResultadoIMC(double altura, double peso) throws RemoteException;
-}
--- projeto2_ToDo_List_SD/tasks_list.py
+++
@@ -1,5 +0,0 @@
-tasks = [
-    {"id": 1, "descricao": "Fazer exercicios fisicos", "status": "concluida"},
-    {"id": 2, "descricao": "Assistir videoaulas", "status": "pendente"},
-    {"id": 3, "descricao": "Resolver lista de exercicios", "status": "pendente"},
-]
